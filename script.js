6c75e3551a78ffe5a3b13ccf0a488a27f9f00759
babaduuuk <92731935+babaduuuk@users.noreply.github.com>
2025-06-13 13:01:07 +0700

    add getComputerChoice()

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("Hello World");
+function getComputerChoice() {
+  const rand = Math.random(); // 0 ≤ rand < 1
+
+  if (rand < 1/3)     return "rock";
+  else if (rand < 2/3) return "paper";
+  else                return "scissors";
+}
+
+// quick test
+console.log(getComputerChoice());
